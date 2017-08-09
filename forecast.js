897d3b87e5d71c4fa31d8b234a4efa841d51cfd7
Blake Guilloud <blake.guilloud@gmail.com>
2017-08-09 18:42:32 -0400

    Initial commit.

---
+++ forecast.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const DarkSky = require('forecast.io');
+
+const client = new DarkSky({
+  APIKey: process.env.DARK_SKY_API_KEY,
+});
+
+module.exports = (latitude, longitude) => {
+  const options = {};
+
+  return new Promise((resolve, reject) => {
+    client.get(latitude, longitude, options, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
